f7bae8d04c0bbe3b983e64c75646fab352442f2b
ecorralro <ecorralr689@alumnos.imf.com>
2023-11-21 13:39:59 +0100

    Create 00-hello.py

---
+++ 00-hello.py
@@ -0,0 +1,13 @@
+# Hola mundo
+print("Hola Python")
+"""
+comentario 
+multilinea
+"""
+# Consultar tipo de dato. Función predeterminada de Python
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType' 
